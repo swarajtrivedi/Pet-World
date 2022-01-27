b63ccb43e7cf61e9a85ba1b410949c0a12fb1a1c
Swaraj Trivedi <swaraj.t@lsq.net>
2022-01-27 17:40:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function toDog(){ //function to view dog image 
+    document.getElementById("btnimg").src="dog.jpg";
+}
+function toCat(){ //function to view cat image
+    document.getElementById("btnimg").src="cat.jpg";
+}
+function toPrt(){ //funciton to view Parrot image
+    document.getElementById("btnimg").src="parrot.jpg";
+}
