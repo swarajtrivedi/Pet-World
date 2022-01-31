c4c51aff26d2a6c92e5c9892fc3bbd96d1ef4f06
swarajtrivedi <47222754+swarajtrivedi@users.noreply.github.com>
2022-01-31 11:20:17 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function toDog(){ //function to view dog image 
-    document.getElementById("btnimg").src="./Assets/images/dog.jpg";
-}
-function toCat(){ //function to view cat image
-    document.getElementById("btnimg").src="./Assets/images/cat.jpg";
-}
-function toPrt(){ //function to view Parrot image
-    document.getElementById("btnimg").src="./Assets/images/parrot.jpg";
-}
