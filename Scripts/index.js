6f8a3cd7d24b2a19b588e5c3f9b02862730aeca7
swarajtrivedi <47222754+swarajtrivedi@users.noreply.github.com>
2022-01-31 11:20:03 +0530

    Create index.js

---
+++ Scripts/index.js
@@ -0,0 +1,9 @@
+function toDog(){ //function to view dog image 
+    document.getElementById("btnimg").src="./Assets/images/dog.jpg";
+}
+function toCat(){ //function to view cat image
+    document.getElementById("btnimg").src="./Assets/images/cat.jpg";
+}
+function toPrt(){ //function to view Parrot image
+    document.getElementById("btnimg").src="./Assets/images/parrot.jpg";
+}
